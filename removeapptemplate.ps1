3f248605c1a6b04996702e273909a8cb565583d3
VietTheBarbarian <56415307+VietTheBarbarian@users.noreply.github.com>
2023-06-08 11:44:51 -0400

    Add files via upload

---
+++ removeapptemplate.ps1
@@ -0,0 +1,11 @@
+﻿if (Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Dell Command | Update for Windows Universal"}) {
+$MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Dell Command | Update for Windows Universal"}
+$MyApp.Uninstall()
+}
+ElseIf (Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Dell Command | Dell Command | Update for Windows 10"}) {
+$MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Dell Command | Dell Command | Update for Windows 10"}
+$MyApp.Uninstall()
+}
+else {
+Write-Host "hl"
+}
